0f88a30d0363cc2a05b9729400a2ce0b9594f217
Hugo Oliveira <hg.oliveira2ds@gmail.com>
2025-04-14 11:26:46 -0300

    Feat: Atualização do dataframe dos países

--- análise.py
+++ análise.py
@@ -53,15 +53,36 @@ plt.ylabel('Perdas Financeiras (em milhões de dólares)')
 plt.show()
 
 
-# Quais os países mais afetados?
-countries_affected = df['Country'].value_counts()
-countries_affected
+# Análises por país
 
-plt.bar(countries_affected.index, countries_affected.values, color='red')
+df_countries = df.groupby('Country').agg(
+    AttackFrequency = ('Country', 'count'),
+    FinancialLoss = ('Financial Loss (in Million $)', 'sum'),
+    )
+
+most_common_attack = df.groupby('Country')['Attack Type'].agg(lambda x: x.mode().iloc[0])
+most_affected_industry = df.groupby('Country')['Target Industry'].agg(lambda x: x.mode().iloc[0])
+most_common_attack_source = df.groupby('Country')['Attack Source'].agg(lambda x: x.mode().iloc[0])
+
+df_countries['MostCommonAttack'] = most_common_attack
+df_countries['MostAffectedIndustry'] = most_affected_industry
+df_countries['MostCommonAttackSource'] = most_common_attack_source
+
+df_countries
+
+# Quais os países mais afetados?
+
+plt.bar(df_countries.index, df_countries['AttackFrequency'], color='red')
 plt.xlabel('Países')
 plt.ylabel('Quantidade de ataques')
 plt.xticks(rotation=45)
 plt.show()
 
+# Quais países tiveram maiores prejuízos financeiros 
 
-# 
+plt.bar(df_countries.index, df_countries['FinancialLoss'], color='blue')
+plt.xlabel('Países')
+plt.ylabel('Perdas Financeiras (em milhoes de dólares')
+plt.xticks(rotation=45)
+plt.show()
+
